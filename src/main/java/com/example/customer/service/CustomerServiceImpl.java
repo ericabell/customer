d20ea919427e1a3870c9e9bebacab8c1938248d2
Eric Abell <ericabell@users.noreply.github.com>
2017-09-26 22:14:25 -0500

    Implement CustomerServiceImpl as passthrough to CustomerRepository

--- src/main/java/com/example/customer/service/CustomerServiceImpl.java
+++ src/main/java/com/example/customer/service/CustomerServiceImpl.java
@@ -3,35 +3,41 @@ package com.example.customer.service;
 import com.example.customer.model.Customer;
 import com.example.customer.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
 public class CustomerServiceImpl implements CustomerService {
     @Autowired
     CustomerRepository customerRepository;
-    
-    @Override
-    public void addCustomer(Customer c) {
 
+    @Override
+    @Transactional
+    public void addCustomer(Customer c) {
+        customerRepository.addCustomer(c);
     }
 
     @Override
+    @Transactional
     public void updateCustomer(Long id, Customer c) {
-
+        customerRepository.updateCustomer(id, c);
     }
 
     @Override
     public Customer findCustomerById(Long id) {
-        return null;
+        return customerRepository.findCustomerById(id);
     }
 
     @Override
     public List<Customer> getAllCustomers() {
-        return null;
+        return customerRepository.getAllCustomers();
     }
 
     @Override
+    @Transactional
     public void deleteCustomer(Long id) {
-
+        customerRepository.deleteCustomer(id);
     }
 }
