070b133d552d041934e8425a4e2be0a4d273b9d2
Eric Abell <ericabell@users.noreply.github.com>
2017-09-28 16:14:05 -0500

    Add customer form and controller work to add a new customer

--- src/main/java/com/example/customer/CustomerController.java
+++ src/main/java/com/example/customer/CustomerController.java
@@ -6,7 +6,10 @@ import com.example.customer.service.CustomerServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,5 +40,24 @@ public class CustomerController {
     @RequestMapping("/add-customer")
     public String addCustomer(Model model) {
         return "add-customer";
+    }
+
+    @PostMapping("/add-customer")
+    public String addCustomerPost(
+            @RequestParam(value="firstname", required=true) String firstname,
+            @RequestParam(value="lastname", required=true) String lastname,
+            @RequestParam(value="email", required=true) String email,
+            @RequestParam(value="phone", required=true) String phone
+            ) {
+        System.out.println("add-customer post");
+        Customer newCustomer = new Customer();
+        newCustomer.setFirstName(firstname);
+        newCustomer.setLastName(lastname);
+        newCustomer.setPhone(phone);
+        newCustomer.setEmail(email);
+
+        customerService.addCustomer(newCustomer);
+
+        return "redirect:/";
     }
 }
