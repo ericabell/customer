3603fe38e3d535f3153b8eadf8fc32ab601bf023
Eric Abell <ericabell@users.noreply.github.com>
2017-09-27 22:29:24 -0500

    customers endpoint displays all customers as a table

--- src/main/java/com/example/customer/CustomerController.java
+++ src/main/java/com/example/customer/CustomerController.java
@@ -1,13 +1,35 @@
 package com.example.customer;
 
+import com.example.customer.model.Customer;
+import com.example.customer.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class CustomerController {
+    @Autowired
+    CustomerService customerService;
+
     @RequestMapping("/")
     public String index(Model model) {
         return "index";
+    }
+
+    @RequestMapping("/customers")
+    public String customers(Model model) {
+        // get all the customer data
+        List<Customer> customers = new ArrayList<>();
+        customers = customerService.getAllCustomers();
+
+        // populate the model
+        model.addAttribute("customers", customers);
+
+        //return the template
+        return "customers";
     }
 }
